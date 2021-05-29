e62d4e672de8554959b75ac885b4bb689cd626c8
WooheonHong <quasar103@naver.com>
2021-05-29 23:04:07 +0900

    add merge k sorted list

---
+++ python_algorithm_interview/merge-k-sorted-lists.py
@@ -0,0 +1,43 @@
+import collections
+import heapq
+from typing import List
+
+
+class ListNode:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        return self.val
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        root = result = ListNode(None)
+        heap = []
+
+        # 각 연결 리스트의 루트를 힙에 저장 
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i, lists[i]))
+
+        while heap:
+            node = heapq.heappop(heap)
+            idx = node[1]
+            result.next = node[2]
+
+            result = result.next 
+            if result.next:
+                heapq.heappush(heap, (result.next.val, idx, result.next))
+
+        return root.next 
+
+
+if __name__ == "__main__":
+    lists = [[1,4,5],[1,3,4],[2,6]]
+    for i, ls in enumerate(lists):
+        for j in range(len(ls) - 1):
+            lists[i] = ListNode(ls[j], ListNode(ls[j+1]))
+
+    s = Solution()
+    s.mergeKLists(lists)
