4a1b1c479b2e7eb4d36393d15b13ab177024256e
WooheonHong <quasar103@naver.com>
2021-05-30 19:22:28 +0900

    add annotation

--- python_algorithm_interview/merge-k-sorted-lists.py
+++ python_algorithm_interview/merge-k-sorted-lists.py
@@ -1,3 +1,10 @@
+"""
+리스트 수 : k
+리스트 원소 수 : n 
+힙에 원소를 힙에 넣었다 뺀다 : 2logn 
+--> O(2knlogkn)
+"""
+
 import collections
 import heapq
 from typing import List
@@ -11,12 +18,13 @@ class ListNode:
     def __repr__(self):
         return self.val
 
+
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         root = result = ListNode(None)
         heap = []
 
-        # 각 연결 리스트의 루트를 힙에 저장 
+        # 각 연결 리스트의 루트를 힙에 저장
         for i in range(len(lists)):
             if lists[i]:
                 heapq.heappush(heap, (lists[i].val, i, lists[i]))
@@ -26,18 +34,18 @@ class Solution:
             idx = node[1]
             result.next = node[2]
 
-            result = result.next 
+            result = result.next
             if result.next:
                 heapq.heappush(heap, (result.next.val, idx, result.next))
 
-        return root.next 
+        return root.next
 
 
 if __name__ == "__main__":
-    lists = [[1,4,5],[1,3,4],[2,6]]
+    lists = [[1, 4, 5], [1, 3, 4], [2, 6]]
     for i, ls in enumerate(lists):
         for j in range(len(ls) - 1):
-            lists[i] = ListNode(ls[j], ListNode(ls[j+1]))
+            lists[i] = ListNode(ls[j], ListNode(ls[j + 1]))
 
     s = Solution()
     s.mergeKLists(lists)
