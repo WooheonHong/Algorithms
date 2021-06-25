d9db4c2c9061bfe56ca28d2360c4becb266c23e2
WooheonHong <quasar103@naver.com>
2021-06-25 16:26:37 +0900

    add some dfs problems

--- python_algorithm_interview/temp.py
+++ python_algorithm_interview/temp.py
@@ -1,22 +1,8 @@
-from typing import List
-from collections import deque
+import copy
 
-graph_list = {
-    1: set([2, 3, 4]),
-    2: set([5]),
-    3: set([5]),
-    4: set([]),
-    5: set([6, 7]),
-    6: set([]),
-    7: set([3]),
-}
-root_node = 1
+a = "사랑해"
+b = a
+c = a[:]
+d = copy.deepcopy(a)
 
-
-def dfs_recursive(v, visited: List):
-    visited.append(v)
-    for w in graph_list[v]:
-        if w not in visited:
-            visited = dfs_recursive(w, visited)
-
-    return visited
+print(id(a), id(b), id(c), id(d))
