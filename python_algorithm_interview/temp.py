ba0f0314fe30c27e2b0d539106da6c6cbf0e186d
WooheonHong <quasar103@naver.com>
2021-07-08 17:35:34 +0900

    reconstruct itinerary

--- python_algorithm_interview/temp.py
+++ python_algorithm_interview/temp.py
@@ -1,8 +1,14 @@
-import copy
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = collections.defaultdict(list)
+        # 그래프 순서대로 구성
+        for a, b in sorted(tickets, reverse=True):
+            graph[a].append(b)
 
-a = "사랑해"
-b = a
-c = a[:]
-d = copy.deepcopy(a)
+        route, stack = [], ["JFK"]
+        while stack:
+            while graph[stack[-1]]:
+                stack.append(graph[stack[-1]].pop(0))
+            route.append(stack.pop())
 
-print(id(a), id(b), id(c), id(d))
+        return route[::-1]
